025608f10fe5c3ee5eed086bf0146d5b2582e645
paulndungu <p.ndungu.kara@gmail.com>
2020-06-30 15:52:45 +0300

    Added the priorities code

---
+++ src/main.kt
@@ -0,0 +1,26 @@
+fun main(){
+
+   var n1=20
+    var n2=40
+    var n3=60
+
+    var sum:Int?
+    sum=n1+n2+n3
+    println(sum)
+    var sum2:Int?
+    sum2=n1+n2/n3
+    println(sum2)
+    var sum3:Int?
+    sum3=n1*n2/(n3-n2)
+    println(sum3)
+
+    var a=60F
+     var b=30F
+      var ans:Float?
+       ans=a+(b/a)
+ println(ans)
+
+
+}
+
+//the math operations use the bodmas rule
